40ba496fb1f2eb903ece35a67395e1f2c73f082d
Ethan <ethanlam93@gmail.com>
2021-01-30 18:28:41 -0500

    update withRouter

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact={true} path="/" component={Homepage} />
+        <Route exact={true} path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
