d20977a18885c2bdfeb6fc1fbeac1a6e64508568
Ethan <ethanlam93@gmail.com>
2021-02-03 17:41:08 -0500

    header component and Links to home

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
-
+import Header from "./components/header/header.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact={true} path="/" component={Homepage} />
         <Route exact={true} path="/shop" component={ShopPage} />
