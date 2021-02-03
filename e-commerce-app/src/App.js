9b489320bd58deea8ec8bedc784643dec1eace99
Ethan <ethanlam93@gmail.com>
2021-02-03 17:16:01 -0500

    collection preview and collection item componenet

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
 function App() {
   return (
     <div>
       <Switch>
         <Route exact={true} path="/" component={Homepage} />
-        <Route exact={true} path="/hats" component={HatsPage} />
+        <Route exact={true} path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
