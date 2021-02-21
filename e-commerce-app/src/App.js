0084e6d11352dc833c05c091478e430e69f96428
Ethan <ethanlam93@gmail.com>
2021-02-20 23:22:33 -0500

    create sign-in with google

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -4,16 +4,38 @@ import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/header/header.component";
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact={true} path="/" component={Homepage} />
-        <Route exact={true} path="/shop" component={ShopPage} />
-      </Switch>
-    </div>
-  );
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./firebase/firebase.util";
+
+class App extends React.Component {
+  state = {
+    currentUser: null,
+  };
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact={true} path="/" component={Homepage} />
+          <Route exact={true} path="/shop" component={ShopPage} />
+          <Route exact={true} path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ e-commerce-app/src/components/sign-in/sign-in.component.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./sign-in.styles.scss";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import { signInWithGoogle } from "../../firebase/firebase.util";
+class SignIn extends React.Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            value={this.state.email}
+            required
+            label="email"
+            handleChange={this.handleChange}
+          />
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="password"
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In With Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
