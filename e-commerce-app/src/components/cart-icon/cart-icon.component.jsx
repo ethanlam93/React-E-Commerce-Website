4468547e298ee9e758be1685935fbc35a1d26efe
Ethan <ethanlam93@gmail.com>
2021-08-06 19:46:08 -0400

    updating our app with reselect npm, replace cart selector with memorized selectors

--- e-commerce-app/src/components/cart-icon/cart-icon.component.jsx
+++ e-commerce-app/src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,7 @@
 import React from "react"   
 import { connect } from 'react-redux';
 import "./cart-icon.styles.scss"
-
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg"
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
@@ -20,8 +20,8 @@ const mapDispatchToProps = dispatch => ({
         toggleCartHidden: () => dispatch(toggleCartHidden())
       });
 
-const mapStateToProps = ({cart : {cartItems }}) => ({
-    itemCount: cartItems.reduce((accumulator, cartItem ) => (accumulator + cartItem.quantity),0)
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
 })
 // const mapStateToProps = createStructuredSelector({
 //     itemCount: selectCartItemsCount
