b758a2722920abb378ad9255335a4de721e94c8d
Ethan <ethanlam93@gmail.com>
2021-08-14 14:11:51 -0400

    add RemoveItem button to checkout

--- e-commerce-app/src/components/checkout-item/checkout-item.component.jsx
+++ e-commerce-app/src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 
 import "./checkout-item.styles.scss";
-
-const CheckoutItem = ({ cartItem: { name, quantity, price, imageUrl } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img alt="item" src={imageUrl} />
+import { removeItem } from "../../redux/cart/cart.actions";
+import { connect } from "react-redux";
+const CheckoutItem = ({ cartItem, removeItemFromCart }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  console.log(cartItem);
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img alt="item" src={imageUrl} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div
+        className="remove-button"
+        onClick={() => removeItemFromCart(cartItem)}
+      >
+        {" "}
+        &#10008;{" "}
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button"> &#10008; </div>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  removeItemFromCart: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
