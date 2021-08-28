f35b5644f1a8a0fbf515a6aea9bd42622685841e
Ethan <ethanlam93@gmail.com>
2021-08-28 16:45:12 -0400

    move Directory state to Redux

--- e-commerce-app/src/redux/root-reducer.js
+++ e-commerce-app/src/redux/root-reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 
 import userReducer from "./user/user-reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
 
@@ -14,6 +15,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
