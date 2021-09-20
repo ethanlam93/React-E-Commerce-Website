537e02dacd447a09b6de48e41eb430eac628e813
Ethan <ethanlam93@gmail.com>
2021-09-20 13:42:33 -0400

    collections state intoRedux and Reselect

--- e-commerce-app/src/pages/shop/shop.component.js
+++ e-commerce-app/src/pages/shop/shop.component.js
@@ -1,22 +1,21 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { selectCollections } from "../../redux/shop/shop.selector";
+import { createStructuredSelector } from "reselect";
+import { connect } from "react-redux";
 
-class ShopPage extends React.Component {
-  state = {
-    collections: SHOP_DATA,
-  };
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
--- e-commerce-app/src/redux/root-reducer.js
+++ e-commerce-app/src/redux/root-reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 
 import userReducer from "./user/user-reducer";
 import cartReducer from "./cart/cart.reducer";
+import shopReducer from "./shop/shop.reducer";
 import directoryReducer from "./directory/directory.reducer";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
@@ -16,6 +17,7 @@ const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
